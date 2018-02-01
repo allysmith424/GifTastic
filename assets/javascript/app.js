357f06a25690ba53d383be74df81f84b6ee64f43
Ally Smith <allysmith@Allys-MacBook-Air.local>
2018-02-01 16:14:24 -0600

    final files for API search assignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,27 @@
-var danceMoves = ["Techtonik", "Hammertime", "Moonwalk", "Running Man", "Macarena"];
+var danceMoves = ["the twist", "the worm", "the robot", "hammertime", "moonwalk", "running man", "macarena"];
 
 var queryURL =  "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=";
 
 function loadGifs() {
 
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        searchTerm + "&api_key=dc6zaTOxFJmzC&limit=5";
+        searchTerm + "&api_key=dc6zaTOxFJmzC&limit=20";
 
     $.ajax({
     	url: queryURL,
     	method: "GET"
     }).then(function(response){
 
-    	console.log(response);
-
     	for (var i = 0; i < response.data.length; i++) {
 
-    		if (response.data[i].rating === "g" || "pg") {
+    		if (response.data[i].rating !== "pg-13" && "r") {
 
 	    		var danceMoveDiv = $("<div>");
 	    		var p = $("<p>");
-	    		p.append(response.data[i].rating);
+	    		p.append((response.data[i].rating).toUpperCase());
 	    		danceMoveImage = $("<img>");
-	    		danceMoveImage.attr("src", response.data[i].images.fixed_height.url).attr("alt", response.data[i].title);
-	    		danceMoveDiv.append(p, danceMoveImage).addClass("danceMoveBtn");
+	    		danceMoveImage.addClass("gif").attr("src", response.data[i].images.fixed_height_small.url).attr("alt", response.data[i].title).attr("data-state", "animate").attr("data-animate", response.data[i].images.fixed_height_small.url).attr("data-still", response.data[i].images.fixed_height_small_still.url);
+	    		danceMoveDiv.append(p, danceMoveImage);
 	    		$("#gifs").append(danceMoveDiv);
 
     		}
@@ -31,7 +29,6 @@ function loadGifs() {
     	}
 
     });
-
 
 };
 
@@ -52,31 +49,50 @@ $(document).ready(function() {
 
 	$(document).on("click", ".danceMoveBtn", function() {
 
-		console.log("dance move button");
-
 		searchTerm = $(this).attr("data-name");
 		$("#gifs").empty();
 		loadGifs();
 
 	});
 
-	$("#searchBtn").on("click", function(event) {
+	$("#addBtn").on("click", function(event) {
 
-		if ($("#searchInput").val() !== "" && danceMoves.indexOf($("#searchInput").val()) === -1) {
-
-			console.log("search button");
+		if ($("#searchInput").val().trim() !== "" && danceMoves.indexOf(($("#searchInput").val()).toLowerCase()) === -1 && $("#suggestedMoves option:selected").val(1)) {
 
 			event.preventDefault();
-			danceMoves.push($("#searchInput").val());
+			danceMoves.push(($("#searchInput").val()).toLowerCase());
 			renderButtons();
 
 		}
 
+		else if ($("#suggestedMoves option:selected").val() !== "1" && danceMoves.indexOf(($("#suggestedMoves option:selected").val()).toLowerCase()) === -1) {
+			event.preventDefault();
+			danceMoves.push(($("#suggestedMoves option:selected").text()).toLowerCase());
+			renderButtons();
+		}
+
 	});
+
+	$(document).on("click", ".gif", function() {
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        }
+        else if (state === "animate") {
+        	console.log(state);
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+
+    });
+
 
 	renderButtons();
 
 });
-	
+
 
 // 
